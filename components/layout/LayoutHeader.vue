31320832384f63028c336670e8c8f63e730ad817
Luis <hi@ernane.me>
2023-05-08 20:35:59 -0300

    show examples of both balances been query: via hooks and via wagmi core generated

--- components/layout/LayoutHeader.vue
+++ components/layout/LayoutHeader.vue
@@ -13,7 +13,10 @@
 
       <div v-if="address" class="flex md:order-2">
         <div class="mr-4">{{ address }}</div>
-        <div class="text-white mr-4">Balance: {{ cashBalance }} $ABC</div>
+        <div class="text-white mr-4">
+          Balance: {{ abcBalance?.formatted }} $ABC
+        </div>
+
         <button @click="() => disconnect()">Disconnect</button>
       </div>
       <div v-else class="flex md:order-2">
@@ -24,16 +27,15 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
-import { useAccount, useDisconnect } from "use-wagmi";
+import { ref, toRefs } from "vue";
+import { useAccount, useDisconnect, useBalance } from "use-wagmi";
 import { readErc20Mock } from "~/lib/generated";
 
 const cashBalance = ref();
 
 const { disconnect } = useDisconnect();
 const { address } = useAccount({
-  onConnect: async (data) => {
-    console.log({ data });
+  onConnect: async () => {
     cashBalance.value = await readErc20Mock({
       address: "0x8335Af67C928Ff9D4f9BE905de767cf252A83fe1",
       functionName: "balanceOf",
@@ -41,5 +43,15 @@ const { address } = useAccount({
     });
   },
   onDisconnect: () => console.log("disconnected"),
+});
+
+const {
+  data: abcBalance,
+  isError,
+  isLoading,
+} = useBalance({
+  address: "0x31DCb7AE01fFfD9B6468814bA2A6A0ab9c58d8e5",
+  token: "0x8335Af67C928Ff9D4f9BE905de767cf252A83fe1",
+  watch: true,
 });
 </script>
