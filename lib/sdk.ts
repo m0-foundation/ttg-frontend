fc8e6435a13785b897109e889d8f3ba07806ecd7
Luis <hi@ernane.me>
2023-05-23 22:41:55 -0300

    update sdk add more types and refactor proposal type, create new function get proposal state

--- lib/sdk.ts
+++ lib/sdk.ts
@@ -1,17 +1,43 @@
-import { createPublicClient, http, decodeEventLog } from "viem";
-import { spogABI, ispogGovernorABI } from "./generated";
+import {
+  Chain,
+  createPublicClient,
+  decodeEventLog,
+  Hash,
+  http,
+  Log,
+  parseAbiItem,
+  PublicClient,
+} from "viem";
+import { BigNumber } from "ethers";
+import type { Abi, AbiParameter, ExtractAbiEventNames, Narrow } from "abitype";
+import { spogABI, ispogGovernorABI, readGovernor } from "./generated";
 
-export interface M0EventLog {
+export interface EventLog {
   eventName: string;
-  description: string;
-  proposalId: BigInt;
   calldatas: Array<string>;
   endBlock: BigInt;
   startBlock: BigInt;
-  proposer: string;
   signatures: Array<string>;
   targets: Array<string>;
   values: Array<any>;
+}
+
+enum ProposalState {
+  Pending = 0,
+  Active = 1,
+  Canceled = 2,
+  Defeated = 3,
+  Succeeded = 4,
+  Queued = 5,
+  Expired = 6,
+  Executed = 7,
+}
+
+export interface MProposal extends EventLog {
+  proposer: string;
+  proposalId: BigInt;
+  description: string;
+  state?: ProposalState;
 }
 
 export interface ConfigVars {
@@ -31,41 +57,64 @@ export interface ConfigVars {
 }
 
 export class SPOG {
-  client: Client;
+  client: PublicClient;
+  config: ConfigVars;
 
   constructor(rpcUrl: string, chain: Chain, config: ConfigVars) {
     this.client = createPublicClient({ chain, transport: http(rpcUrl) });
     this.config = config;
   }
 
-  decodeRawLog(log: object, abi: object) {
+  decodeProposalLog(log: Log, abi: object): MProposal {
     const { eventName, args } = decodeEventLog({
       abi,
       data: log?.data,
       topics: log?.topics,
     });
-    return { eventName, ...args };
+
+    const proposal: MProposal = {
+      eventName,
+      ...args,
+    };
+    return proposal;
   }
 
-  async getGovernorVoteProposals(
-    fromBlock?: BigInt = BigInt(this.config.deployedBlock)
-  ): Promise<M0EventLog[]> {
+  async getGovernorVoteProposals(): Promise<MProposal> {
+    const deployedBlock: BigInt = BigInt(this.config.deployedBlock.toString());
+
     const rawLogs = await this.client.getLogs({
-      address: this.config.governor.vote,
-      fromBlock,
+      address: this.config.governor.vote as Hash,
+      fromBlock: deployedBlock,
     });
 
-    return rawLogs.map((log) => this.decodeRawLog(log, ispogGovernorABI));
+    return rawLogs.map((log: Log) =>
+      this.decodeProposalLog(log, ispogGovernorABI)
+    );
   }
 
-  async getSpogProposals(
-    fromBlock?: BigInt = BigInt(this.config.deployedBlock)
-  ): Promise<M0EventLog[]> {
+  async getSpogProposals(): Promise<MProposal[]> {
+    const deployedBlock: BigInt = BigInt(this.config.deployedBlock.toString());
+
     const rawLogs = await this.client.getLogs({
-      address: this.config.spog,
-      fromBlock,
+      address: this.config.spog as Hash,
+      fromBlock: deployedBlock,
+      event: parseAbiItem(
+        "event ProposalCreated(uint256 proposalId, address proposer, address[] targets, uint256[] values, string[] signatures, bytes[] calldatas, uint256 voteStart, uint256 voteEnd, string description)"
+      ),
     });
 
-    return rawLogs.map((log) => this.decodeRawLog(log, spogABI));
+    return rawLogs.map((log: Log) => this.decodeProposalLog(log, spogABI));
+  }
+
+  async getProposalState(
+    proposalId: string
+  ): Promise<keyof typeof ProposalState> {
+    const proposalStateNumber = await readGovernor({
+      address: this.config.governor.vote as Hash,
+      functionName: "state",
+      args: [BigNumber.from(proposalId)],
+    });
+
+    return ProposalState[proposalStateNumber] as keyof typeof ProposalState;
   }
 }
