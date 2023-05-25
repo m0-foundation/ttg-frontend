89b705d29ba9b787e82364f7d26b144d9c8f2022
Luis <hi@ernane.me>
2023-05-25 14:41:25 -0300

    show date on card proposal

--- components/proposal/Card.vue
+++ components/proposal/Card.vue
@@ -3,49 +3,44 @@
     <h2 class="text-4xl mb-4">
       {{ title }}
     </h2>
-    <div class="text-primary-darker text-sm mb-6">
-      Proposed by <u>{{ proposer }}</u>
+    <div class="text-primary-darker text-sm mb-6 flex justify-between">
+      <div>
+        Proposed by <u>{{ proposal.proposer }}</u>
+      </div>
+      <div>{{ timeAgo }} | {{ formatedDate }}</div>
     </div>
     <div class="markdown-body mb-6" v-html="descriptionShort"></div>
     <div class="flex justify-between uppercase">
-      <NuxtLink class="uppercase" :to="`/proposals/${proposalId}`">
+      <NuxtLink
+        class="uppercase border-b border-black border-dashed"
+        :to="`/proposals/${proposal.proposalId}`"
+      >
         Show Details
       </NuxtLink>
 
-      <!-- <div class="flex justify-between items-center">
+      <div class="flex justify-between items-center">
         <div class="text-primary-darker text-sm mr-4">
-          Active | participation rate
+          {{ proposal.state }}
         </div>
-        <div class="space-x-1">
-          <MButton>YES</MButton>
-          <MButton>NO</MButton>
-        </div>
-      </div> -->
+      </div>
     </div>
   </article>
 </template>
 
-<script setup>
-const props = defineProps({
-  proposalId: {
-    type: String,
-    default: "",
-  },
-  description: {
-    type: String,
-    default: "",
-  },
-  proposer: {
-    type: String,
-    default: "",
-  },
-  url: {
-    type: String,
-    default: "",
-  },
-});
+<script setup lang="ts">
+import { MProposal } from "~/lib/sdk";
 
-const { html } = useParsedDescription(props.description);
+export interface ProposalCardProps {
+  proposal: MProposal;
+}
+
+const props = defineProps<ProposalCardProps>();
+
+const { format, timeAgo } = useDate(props.proposal.timestamp);
+
+const formatedDate = computed(() => format("LL"));
+
+const { html } = useParsedDescription(props.proposal.description);
 const domParser = new DOMParser();
 
 const title = computed(() => {
