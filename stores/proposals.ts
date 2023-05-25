1a57a83b7295109496fe35123cc0096866085e6b
Luis <hi@ernane.me>
2023-05-25 14:39:32 -0300

    add store proposals to reactive local storage + sorting proposals by blocknumber desc

--- stores/proposals.ts
+++ stores/proposals.ts
@@ -1,21 +1,28 @@
 import { defineStore } from "pinia";
+import _ from "lodash";
 import { MProposal } from "@/lib/sdk";
 
 export const useProposalsStore = defineStore("proposals", {
   state: () => ({
-    data: [] as MProposal[],
+    data: useLocalStorage("m0.proposals", [] as MProposal[]),
   }),
 
   getters: {
     getProposals: (state) => state.data,
+
+    getProposalById: (state) => {
+      return (pId: string) =>
+        state.data.find((p) => p.proposalId.toString() === pId);
+    },
   },
 
   actions: {
-    setProposals(proposals: MProposal[]) {
-      this.data = proposals;
-    },
-    getProposalById(pId: string) {
-      return this.data.find((p) => p.proposalId.toString() === pId);
+    setProposals(proposals: Array<MProposal>) {
+      this.data = _.orderBy(
+        _.uniqBy([...this.data, ...proposals], "proposalId"),
+        "blockNumber",
+        "desc"
+      );
     },
   },
 });
