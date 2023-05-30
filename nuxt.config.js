8aa90d93a5dd0fe69f7ca40203faafd248cfb6a6
Luis <hi@ernane.me>
2023-05-30 14:44:09 -0300

    clean env vars and create env.example

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,6 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-      WALLET_CONNECT_PROJECT_ID: process.env.WALLET_CONNECT_PROJECT_ID,
-      ALCHEMY_API_KEY: process.env.ALCHEMY_API_KEY,
-      INFURA_API_KEY: process.env.INFURA_API_KEY,
       network: {
         defaultRpc: process.env.NETWORK_DEFAULT_RPC,
         rpcs: process.env.NETWORK_RPC_LIST,
