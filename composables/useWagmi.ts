0f0b146f0eebfb4f918029b0d4b69eca0d8f1999
Luis <hi@ernane.me>
2023-05-30 14:33:43 -0300

    rename useEthereum to useWagmi

--- composables/useWagmi.ts
+++ composables/useWagmi.ts
@@ -1,6 +1,48 @@
-export const useWagmi = () => {
-  const nuxtApp = useNuxtApp();
-  const client = nuxtApp.$wagmiClient;
+import { mainnet, sepolia } from "@wagmi/core/chains";
+import { configureChains, InjectedConnector } from "@wagmi/core";
+// connectors
+import { CoinbaseWalletConnector } from "use-wagmi/connectors/coinbaseWallet";
+import { LedgerConnector } from "use-wagmi/connectors/ledger";
+import { MetaMaskConnector } from "use-wagmi/connectors/metaMask";
+import { WalletConnectLegacyConnector } from "use-wagmi/connectors/walletConnectLegacy";
+// RPCs providers
+import { publicProvider } from "@wagmi/core/providers/public";
+import { jsonRpcProvider } from "@wagmi/core/providers/jsonRpc";
+// use wagmi
+import { createClient } from "use-wagmi";
 
+export const useWagmi = (rpc: string, chain = sepolia) => {
+  const { chains, provider, webSocketProvider } = configureChains(
+    [chain], // mainnet, goerli
+    [
+      jsonRpcProvider({
+        rpc: () => ({
+          http: rpc,
+          // webSocket: TODO?
+        }),
+      }),
+      // publicProvider(),
+    ],
+    { targetQuorum: 1 }
+  );
+
+  const connectors = [
+    new MetaMaskConnector({
+      chains,
+      options: {
+        UNSTABLE_shimOnConnectSelectAccount: true,
+      },
+    }),
+    // browsers that inject ethereum such as mobile app and metamask
+    // new InjectedConnector(),
+  ];
+
+  const client = createClient({
+    autoConnect: true,
+    connectors,
+    provider,
+    // webSocketProvider, TODO?
+  });
+  // install wagmi client as vue plugin using wrapper from 'use-wagmi'
   return { client };
 };
