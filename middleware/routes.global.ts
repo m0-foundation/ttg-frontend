9caee80c28432290088649cbe4d4d3acc2aab26e
Luis <hi@ernane.me>
2023-05-30 11:22:53 -0300

    remove redirects from global routes

--- middleware/routes.global.ts
+++ middleware/routes.global.ts
@@ -1,14 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default defineNuxtRouteMiddleware((to, from) => {
-  console.log("routes.global");
-
   if (to.path === "/") {
     return navigateTo("/proposals/active");
   }
   if (to.path === "/lists") {
     return navigateTo("/lists/merchants");
-  }
-
-  if (to.path === "/setup") {
-    return navigateTo("/setup/1");
   }
 });
