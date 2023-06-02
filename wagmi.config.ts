024665941b7d9965aed0063b3c7dfd4be956848c
Luis <hi@ernane.me>
2023-06-01 22:20:41 -0300

    refactor wagmi  sdk generator config file to use new spog contract as yarn link

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,19 +1,14 @@
-import { defineConfig } from '@wagmi/cli'
-import { foundry, actions } from '@wagmi/cli/plugins'
+import { defineConfig } from "@wagmi/cli";
+import { foundry, actions } from "@wagmi/cli/plugins";
 
 export default defineConfig({
-  out: 'src/generated.ts',
+  out: "lib/sdk.ts",
   contracts: [],
   plugins: [
     foundry({
-      project: '../SPOG',
-      deployments: {
-        SPOGFactory: {
-          11155111: '0xA1CE7Bb1C5a9459663Abf8833EC8071296FA0791',
-          31337: '0x4826533B4897376654Bb4d4AD88B7faFD0C98528',
-        },
-      },
+      artifacts: "node_modules/@mzero-labs/spog/out",
+      include: ["I**.sol/*.json"],
     }),
     actions(),
   ],
-})
+});
