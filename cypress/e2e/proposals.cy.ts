814aae63300ad409dc4d9e8fe5321c6cf1eaf6b9
D Midnight <53611782+dmidnight@users.noreply.github.com>
2023-06-13 15:37:51 -0400

    Cleanup CI output

--- cypress/e2e/proposals.cy.ts
+++ cypress/e2e/proposals.cy.ts
@@ -2,7 +2,7 @@ describe("Governance", async () => {
   it("should be able to create a proposal to add a list", () => {
     cy.task("hardhat").then((network) => {
       cy.visit("http://localhost:3000/");
-      cy.get("button").click();
+      cy.get("button").eq(0).click();
       cy.get("div#modal-backdrop").within(() => {
         cy.get("button").eq(1).click();
       });
