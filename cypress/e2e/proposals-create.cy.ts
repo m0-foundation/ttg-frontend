d8744a980ce92ad30c7b9b6a042daa82a0935679
Luis <hi@ernane.me>
2023-06-15 17:48:09 -0300

    create e2e test for updateVoteQuorumNumerator and updateValueQuorumNumerator

--- cypress/e2e/proposals-create.cy.ts
+++ cypress/e2e/proposals-create.cy.ts
@@ -34,4 +34,74 @@ describe("Governance", async () => {
       });
     });
   });
+
+  it("create proposal for type action: updateVoteQuorumNumerator", () => {
+    cy.task("hardhat").then(() => {
+      cy.contains("Select a proposal type").should("exist");
+      cy.contains("Select a proposal type").click();
+
+      cy.contains("Change Quorums").should("exist");
+      cy.contains("Change Quorums").click();
+
+      cy.contains("Vote Quorum").should("exist");
+      cy.contains("Vote Quorum").click();
+
+      cy.get("input[data-test='proposalValue']").should(
+        "have.attr",
+        "type",
+        "number"
+      );
+      cy.get("input[data-test='proposalValue']").type("10");
+
+      cy.get("textarea[name='description']").type(
+        "Change Vote Quorum Numerator to 10%"
+      );
+
+      cy.contains("Preview proposal").should("exist");
+      cy.contains("Preview proposal").click();
+
+      cy.connectWallet();
+
+      cy.contains("Submit proposal").should("exist");
+      cy.contains("Submit proposal").then(($el) => {
+        $el.click();
+        cy.get(".complete").should("have.length", 3);
+      });
+    });
+  });
+
+  it("create proposal for type action: updateValueQuorumNumerator", () => {
+    cy.task("hardhat").then(() => {
+      cy.contains("Select a proposal type").should("exist");
+      cy.contains("Select a proposal type").click();
+
+      cy.contains("Change Quorums").should("exist");
+      cy.contains("Change Quorums").click();
+
+      cy.contains("Value Quorum").should("exist");
+      cy.contains("Value Quorum").click();
+
+      cy.get("input[data-test='proposalValue']").should(
+        "have.attr",
+        "type",
+        "number"
+      );
+      cy.get("input[data-test='proposalValue']").type("10");
+
+      cy.get("textarea[name='description']").type(
+        "Change Value Quorum Numerator to 10%"
+      );
+
+      cy.contains("Preview proposal").should("exist");
+      cy.contains("Preview proposal").click();
+
+      cy.connectWallet();
+
+      cy.contains("Submit proposal").should("exist");
+      cy.contains("Submit proposal").then(($el) => {
+        $el.click();
+        cy.get(".complete").should("have.length", 3);
+      });
+    });
+  });
 });
