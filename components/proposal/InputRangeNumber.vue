06795927d57b7bf9641e4105ca8c74aeb4274469
Luis <hi@ernane.me>
2023-06-30 15:41:27 +0100

    create proposal componets to handle input types based on user selection of type of proposal

---
+++ components/proposal/InputRangeNumber.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="w-full flex justify-between items-center space-x-4">
+    <input
+      v-model="from"
+      class="input"
+      type="number"
+      placeholder="From"
+      data-test="proposalValue"
+    />
+
+    <input
+      v-model="to"
+      class="input"
+      type="number"
+      placeholder="To"
+      data-test="proposalValue2"
+    />
+    <div v-show="props.currentFrom && props.currentTo" class="w-1/2 uppercase">
+      current: {{ props.currentFrom }} - {{ props.currentTo }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+export interface InputProps {
+  currentFrom: string;
+  currentTo: string;
+  modelValue: number;
+  modelValue2: number;
+}
+
+const props = defineProps<InputProps>();
+const emit = defineEmits(["update:modelValue", "update:modelValue2"]);
+
+const from = useVModelWrapper<InputProps>(props, emit, "modelValue");
+const to = useVModelWrapper<InputProps>(props, emit, "modelValue2");
+</script>
---
+++ components/proposal/InputSingleText.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="w-full flex justify-between items-center space-x-4">
+    <input
+      v-model="value"
+      class="input"
+      type="text"
+      data-test="proposalValue"
+      :placeholder="placeholder"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+export interface InputProps {
+  modelValue: string;
+  placeholder: string;
+}
+
+const props = defineProps<InputProps>();
+const emit = defineEmits(["update:modelValue"]);
+
+const value = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value) {
+    emit("update:modelValue", value);
+  },
+});
+</script>
