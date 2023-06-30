e063d44d9d6a0846395046bdfbe54b7cdd89c2df
Luis <hi@ernane.me>
2023-06-30 15:43:19 +0100

    create all tests for create proposals group by operations set

--- cypress/e2e/proposals-create.cy.ts
+++ cypress/e2e/proposals-create.cy.ts
@@ -6,135 +6,394 @@ describe("Proposals", async () => {
       cy.visit("http://localhost:3000/proposals/create");
     });
 
-    it("I should be able to a create proposal for type action: changeTax", () => {
-      cy.contains("Select a proposal type").should("exist");
-      cy.contains("Select a proposal type").click();
+    describe("Is Quorum Operation", () => {
+      it("I should be able to a create proposal for type action: updateVoteQuorumNumerator", () => {
+        cy.contains("Select a proposal type").should("exist");
+        cy.contains("Select a proposal type").click();
 
-      cy.contains("Tax").click();
-      cy.contains("Change Tax").click();
+        cy.contains("Change Quorums").should("exist");
+        cy.contains("Change Quorums").click();
 
-      cy.get("input[data-test='proposalValue']").should(
-        "have.attr",
-        "type",
-        "number"
-      );
-      cy.get("input[data-test='proposalValue']").type("10");
+        cy.contains("Vote Quorum").should("exist");
+        cy.contains("Vote Quorum").click();
 
-      cy.get("textarea[name='description']").type("Change tax to 10x");
+        cy.get("input[data-test='proposalValue']").should(
+          "have.attr",
+          "type",
+          "number"
+        );
+        cy.get("input[data-test='proposalValue']").type("10");
 
-      cy.contains("Preview proposal").should("exist");
-      cy.contains("Preview proposal").click();
+        cy.get("textarea[name='description']").type(
+          "Change Vote Quorum Numerator to 10%"
+        );
 
-      cy.connectWallet();
+        cy.contains("Preview proposal").should("exist");
+        cy.contains("Preview proposal").click();
 
-      cy.contains("Submit proposal").should("exist");
-      cy.contains("Submit proposal").then(async ($el) => {
-        $el.click();
-        cy.get(".complete").should("have.length", 3);
+        cy.connectWallet();
+
+        cy.contains("Submit proposal").should("exist");
+        cy.contains("Submit proposal").then(($el) => {
+          $el.click();
+          cy.get(".complete").should("have.length", 3);
+        });
+      });
+
+      it("I should be able to a create proposal for type action: updateValueQuorumNumerator", () => {
+        cy.contains("Select a proposal type").should("exist");
+        cy.contains("Select a proposal type").click();
+
+        cy.contains("Change Quorums").should("exist");
+        cy.contains("Change Quorums").click();
+
+        cy.contains("Value Quorum").should("exist");
+        cy.contains("Value Quorum").click();
+
+        cy.get("input[data-test='proposalValue']").should(
+          "have.attr",
+          "type",
+          "number"
+        );
+        cy.get("input[data-test='proposalValue']").type("10");
+
+        cy.get("textarea[name='description']").type(
+          "Change Value Quorum Numerator to 10%"
+        );
+
+        cy.contains("Preview proposal").should("exist");
+        cy.contains("Preview proposal").click();
+
+        cy.connectWallet();
+
+        cy.contains("Submit proposal").should("exist");
+        cy.contains("Submit proposal").then(($el) => {
+          $el.click();
+          cy.get(".complete").should("have.length", 3);
+        });
       });
     });
 
-    it("I should be able to a create proposal for type action: changeTaxRange", () => {
-      cy.contains("Select a proposal type").should("exist");
-      cy.contains("Select a proposal type").click();
+    describe("Is Tax Operation", () => {
+      it("I should be able to a create proposal for type action: changeTax", () => {
+        cy.contains("Select a proposal type").should("exist");
+        cy.contains("Select a proposal type").click();
 
-      cy.contains("Tax").click();
-      cy.contains("Change Tax range").click();
+        cy.contains("Tax").click();
+        cy.contains("Change Tax").click();
 
-      cy.get("input[data-test='proposalValue']").type("2");
-      cy.get("input[data-test='proposalValue2']").type("8");
+        cy.get("input[data-test='proposalValue']").should(
+          "have.attr",
+          "type",
+          "number"
+        );
+        cy.get("input[data-test='proposalValue']").type("10");
 
-      cy.get("textarea[name='description']").type(
-        "Change Tax Range to 2x - 8x"
-      );
+        cy.get("textarea[name='description']").type("Change tax to 10x");
 
-      cy.contains("Preview proposal").should("exist");
-      cy.contains("Preview proposal").click();
+        cy.contains("Preview proposal").should("exist");
+        cy.contains("Preview proposal").click();
 
-      cy.connectWallet();
+        cy.connectWallet();
 
-      cy.contains("Submit proposal").should("exist");
-      cy.contains("Submit proposal").then(($el) => {
-        $el.click();
-        cy.get(".complete").should("have.length", 3);
+        cy.contains("Submit proposal").should("exist");
+        cy.contains("Submit proposal").then(async ($el) => {
+          $el.click();
+          cy.get(".complete").should("have.length", 3);
+        });
+      });
+
+      it("I should be able to a create proposal for type action: changeTaxRange", () => {
+        cy.contains("Select a proposal type").should("exist");
+        cy.contains("Select a proposal type").click();
+
+        cy.contains("Tax").click();
+        cy.contains("Change Tax range").click();
+
+        cy.get("input[data-test='proposalValue']").type("2");
+        cy.get("input[data-test='proposalValue2']").type("8");
+
+        cy.get("textarea[name='description']").type(
+          "Change Tax Range to 2x - 8x"
+        );
+
+        cy.contains("Preview proposal").should("exist");
+        cy.contains("Preview proposal").click();
+
+        cy.connectWallet();
+
+        cy.contains("Submit proposal").should("exist");
+        cy.contains("Submit proposal").then(($el) => {
+          $el.click();
+          cy.get(".complete").should("have.length", 3);
+        });
       });
     });
 
-    it("I should be able to a create proposal for type action: updateVoteQuorumNumerator", () => {
-      cy.contains("Select a proposal type").should("exist");
-      cy.contains("Select a proposal type").click();
+    describe("Is List Operation", () => {
+      it("I should be able to a create proposal for type action: Create a new list", () => {
+        cy.contains("Select a proposal type").should("exist");
+        cy.contains("Select a proposal type").click();
 
-      cy.contains("Change Quorums").should("exist");
-      cy.contains("Change Quorums").click();
+        cy.contains("List").should("exist").click();
 
-      cy.contains("Vote Quorum").should("exist");
-      cy.contains("Vote Quorum").click();
+        cy.contains("Create a new list").should("exist");
+        cy.contains("Create a new list").click({ force: true });
 
-      cy.get("input[data-test='proposalValue']").should(
-        "have.attr",
-        "type",
-        "number"
-      );
-      cy.get("input[data-test='proposalValue']").type("10");
+        cy.get("input[data-test='proposalValue']").should(
+          "have.attr",
+          "type",
+          "text"
+        );
 
-      cy.get("textarea[name='description']").type(
-        "Change Vote Quorum Numerator to 10%"
-      );
+        cy.get("input[data-test='proposalValue']").type("Collateral Managers");
 
-      cy.contains("Preview proposal").should("exist");
-      cy.contains("Preview proposal").click();
+        cy.get("textarea[name='description']").type(
+          "Add the Collateral Managers list"
+        );
 
-      cy.connectWallet();
+        cy.contains("Preview proposal").should("exist");
+        cy.contains("Preview proposal").click();
 
-      cy.contains("Submit proposal").should("exist");
-      cy.contains("Submit proposal").then(($el) => {
-        $el.click();
-        cy.get(".complete").should("have.length", 3);
+        cy.connectWallet();
+
+        cy.contains("Submit proposal").should("exist");
+        cy.contains("Submit proposal").then(($el) => {
+          $el.click();
+          cy.get(".complete").should("have.length", 4);
+        });
+      });
+
+      it("I should be able to a create proposal for type action: Append to a list", () => {
+        cy.contains("Select a proposal type").should("exist");
+        cy.contains("Select a proposal type").click();
+
+        cy.contains("List").should("exist").click();
+
+        cy.contains("Append to a list").should("exist").click({ force: true });
+
+        cy.get("input[data-test='proposalValue']").should(
+          "have.attr",
+          "type",
+          "text"
+        );
+        cy.get("input[data-test='proposalValue2']").should(
+          "have.attr",
+          "type",
+          "text"
+        );
+
+        cy.get("input[data-test='proposalValue']").type(
+          "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+        );
+        cy.get("input[data-test='proposalValue2']").type(
+          "0x1fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+        );
+
+        cy.get("textarea[name='description']").type("Append to list");
+
+        cy.contains("Preview proposal").should("exist");
+        cy.contains("Preview proposal").click();
+
+        cy.connectWallet();
+
+        cy.contains("Submit proposal").should("exist");
+        cy.contains("Submit proposal").then(($el) => {
+          $el.click();
+          cy.get(".complete").should("have.length", 3);
+        });
+      });
+
+      it("I should be able to a create proposal for type action: Remove from a list", () => {
+        cy.contains("Select a proposal type").should("exist");
+        cy.contains("Select a proposal type").click();
+
+        cy.contains("List").should("exist").click();
+
+        cy.contains("Remove from a list")
+          .should("exist")
+          .click({ force: true });
+
+        cy.get("input[data-test='proposalValue']").should(
+          "have.attr",
+          "type",
+          "text"
+        );
+        cy.get("input[data-test='proposalValue2']").should(
+          "have.attr",
+          "type",
+          "text"
+        );
+
+        cy.get("input[data-test='proposalValue']").type(
+          "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+        );
+        cy.get("input[data-test='proposalValue2']").type(
+          "0x1fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+        );
+
+        cy.get("textarea[name='description']").type("Remove from a list");
+
+        cy.contains("Preview proposal").should("exist");
+        cy.contains("Preview proposal").click();
+
+        cy.connectWallet();
+
+        cy.contains("Submit proposal").should("exist");
+        cy.contains("Submit proposal").then(($el) => {
+          $el.click();
+          cy.get(".complete").should("have.length", 3);
+        });
       });
     });
 
-    it("I should be able to a create proposal for type action: updateValueQuorumNumerator", () => {
-      cy.contains("Select a proposal type").should("exist");
-      cy.contains("Select a proposal type").click();
+    describe("Is Emergency Proposal", () => {
+      it("I should be able to a create an Emergency proposal for type action: Append", () => {
+        cy.contains("Select a proposal type").should("exist");
+        cy.contains("Select a proposal type").click();
 
-      cy.contains("Change Quorums").should("exist");
-      cy.contains("Change Quorums").click();
+        cy.contains("Emergency").should("exist");
+        cy.contains("Emergency").click();
 
-      cy.contains("Value Quorum").should("exist");
-      cy.contains("Value Quorum").click();
+        cy.contains("Append to a list").should("exist").click({ force: true });
 
-      cy.get("input[data-test='proposalValue']").should(
-        "have.attr",
-        "type",
-        "number"
-      );
-      cy.get("input[data-test='proposalValue']").type("10");
+        cy.get("input[data-test='proposalValue']").should(
+          "have.attr",
+          "type",
+          "text"
+        );
+        cy.get("input[data-test='proposalValue2']").should(
+          "have.attr",
+          "type",
+          "text"
+        );
 
-      cy.get("textarea[name='description']").type(
-        "Change Value Quorum Numerator to 10%"
-      );
+        cy.get("input[data-test='proposalValue']").type(
+          "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+        );
+        cy.get("input[data-test='proposalValue2']").type(
+          "0x1fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+        );
+        // cy.get("input[data-test='proposalValue3']").type("0x01ffc9a7");
 
-      cy.contains("Preview proposal").should("exist");
-      cy.contains("Preview proposal").click();
+        cy.get("textarea[name='description']").type("Emergency append to list");
 
-      cy.connectWallet();
+        cy.contains("Preview proposal").should("exist");
+        cy.contains("Preview proposal").click();
 
-      cy.contains("Submit proposal").should("exist");
-      cy.contains("Submit proposal").then(($el) => {
-        $el.click();
-        cy.get(".complete").should("have.length", 3);
+        cy.connectWallet();
+
+        cy.contains("Submit proposal").should("exist");
+        cy.contains("Submit proposal").then(($el) => {
+          $el.click();
+          cy.get(".complete").should("have.length", 3);
+        });
+      });
+
+      it("I should be able to a create an Emergency proposal for type action: Remove", () => {
+        cy.contains("Select a proposal type").should("exist");
+        cy.contains("Select a proposal type").click();
+
+        cy.contains("Emergency").should("exist");
+        cy.contains("Emergency").click();
+
+        cy.contains("Remove from a list")
+          .should("exist")
+          .click({ force: true });
+
+        cy.get("input[data-test='proposalValue']").should(
+          "have.attr",
+          "type",
+          "text"
+        );
+        cy.get("input[data-test='proposalValue2']").should(
+          "have.attr",
+          "type",
+          "text"
+        );
+
+        cy.get("input[data-test='proposalValue']").type(
+          "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+        );
+        cy.get("input[data-test='proposalValue2']").type(
+          "0x1fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+        );
+
+        cy.get("textarea[name='description']").type(
+          "Emergency remove from a list"
+        );
+
+        cy.contains("Preview proposal").should("exist");
+        cy.contains("Preview proposal").click();
+
+        cy.connectWallet();
+
+        cy.contains("Submit proposal").should("exist");
+        cy.contains("Submit proposal").then(($el) => {
+          $el.click();
+          cy.get(".complete").should("have.length", 3);
+        });
+      });
+
+      it("I should be able to a create an Emergency proposal for type action: Chance Config", () => {
+        cy.contains("Select a proposal type").should("exist");
+        cy.contains("Select a proposal type").click();
+
+        cy.contains("Emergency").should("exist");
+        cy.contains("Emergency").click();
+
+        cy.contains("Change Config").should("exist").click({ force: true });
+
+        cy.get("input[data-test='proposalValue']").should(
+          "have.attr",
+          "type",
+          "text"
+        );
+
+        cy.get("input[data-test='proposalValue2']").should(
+          "have.attr",
+          "type",
+          "text"
+        );
+
+        cy.get("input[data-test='proposalValue3']").should(
+          "have.attr",
+          "type",
+          "text"
+        );
+
+        const configName = "inflator";
+        // config Name
+        cy.get("input[data-test='proposalValue']").type(configName);
+        // config Address
+        cy.get("input[data-test='proposalValue2']").type(
+          "0x1fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+        );
+        // interfaceId
+        cy.get("input[data-test='proposalValue3']").type("0x01ffc9a7");
+
+        cy.get("textarea[name='description']").type(
+          `Emergency change config ${configName}`
+        );
+
+        cy.contains("Preview proposal").should("exist");
+        cy.contains("Preview proposal").click();
+
+        cy.connectWallet();
+
+        cy.contains("Submit proposal").should("exist");
+        cy.contains("Submit proposal").then(($el) => {
+          $el.click();
+          cy.get(".complete").should("have.length", 3);
+        });
       });
     });
 
-    it("I should be able to a create an Emergency proposal for type action: Append", () => {
+    it("I should be able to a create  proposal for type action: Chance Config", () => {
       cy.contains("Select a proposal type").should("exist");
       cy.contains("Select a proposal type").click();
 
-      cy.contains("Emergency").should("exist");
-      cy.contains("Emergency").click();
-
-      cy.contains("Append").should("exist");
-      cy.contains("Append").click();
+      cy.contains("Change Config").should("exist").click({ force: true });
 
       cy.get("input[data-test='proposalValue']").should(
         "have.attr",
@@ -142,14 +401,30 @@ describe("Proposals", async () => {
         "text"
       );
 
-      cy.get("input[data-test='proposalValue']").type("configName");
-      cy.get("input[data-test='proposalValue2']").type(
-        "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+      cy.get("input[data-test='proposalValue2']").should(
+        "have.attr",
+        "type",
+        "text"
       );
+
+      cy.get("input[data-test='proposalValue3']").should(
+        "have.attr",
+        "type",
+        "text"
+      );
+
+      const configName = "protocolFee";
+      // config Name
+      cy.get("input[data-test='proposalValue']").type(configName);
+      // config Address
+      cy.get("input[data-test='proposalValue2']").type(
+        "0x1fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+      );
+      // interfaceId
       cy.get("input[data-test='proposalValue3']").type("0x01ffc9a7");
 
       cy.get("textarea[name='description']").type(
-        "Emergency change configName to 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0 0x01ffc9a7"
+        `Emergency change config ${configName}`
       );
 
       cy.contains("Preview proposal").should("exist");
@@ -164,12 +439,11 @@ describe("Proposals", async () => {
       });
     });
 
-    it("I should be able to a create proposal for type action: addList", () => {
+    it("I should be able to a create  proposal for type action: Reset", () => {
       cy.contains("Select a proposal type").should("exist");
       cy.contains("Select a proposal type").click();
 
-      cy.contains("Create a new list").should("exist");
-      cy.contains("Create a new list").click();
+      cy.contains("Reset").should("exist").click({ force: true });
 
       cy.get("input[data-test='proposalValue']").should(
         "have.attr",
@@ -177,10 +451,11 @@ describe("Proposals", async () => {
         "text"
       );
 
-      cy.get("input[data-test='proposalValue']").type("Collateral Managers");
+      const address = "0x1fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+      cy.get("input[data-test='proposalValue']").type(address);
 
       cy.get("textarea[name='description']").type(
-        "Add the Collateral Managers list"
+        `Reset governance to this contract ${address}`
       );
 
       cy.contains("Preview proposal").should("exist");
@@ -191,7 +466,7 @@ describe("Proposals", async () => {
       cy.contains("Submit proposal").should("exist");
       cy.contains("Submit proposal").then(($el) => {
         $el.click();
-        cy.get(".complete").should("have.length", 4);
+        cy.get(".complete").should("have.length", 3);
       });
     });
   });
