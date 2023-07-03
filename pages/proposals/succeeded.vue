88d2b1e89dadda52805824b3b7889ede8102a718
Luis <hi@ernane.me>
2023-07-03 18:09:59 -0300

    created pending to be executed page aka succeded proposals

---
+++ pages/proposals/succeeded.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ProposalNavbar />
+    <LayoutPage>
+      <div v-if="!proposals || !proposals.length">
+        No Proposals to be executed.
+      </div>
+      <div v-for="proposal in proposals" v-else :key="proposal.proposalId">
+        <ProposalCard :proposal="proposal" />
+      </div>
+    </LayoutPage>
+  </div>
+</template>
+
+<script setup>
+import { storeToRefs } from "pinia";
+
+const proposalsStore = useProposalsStore();
+
+const { getProposalsByState } = storeToRefs(proposalsStore);
+
+const proposals = getProposalsByState.value("Succeeded");
+</script>
