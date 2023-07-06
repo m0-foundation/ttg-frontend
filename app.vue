0bc7f6c7090af2db12bc69602a4e73f1a647741b
dmidnight <53611782+dmidnight@users.noreply.github.com>
2023-07-06 09:22:32 -0400

    Make app use network specified in environment var (#33)
    * Make app use network specified in environment var
    
    * switch filter to find

--- app.vue
+++ app.vue
@@ -40,8 +40,11 @@ function onSetup(rpc: string) {
   nuxtApp.vueApp.use(UseWagmiPlugin, wagmiClient);
 
   /* setup spog client */
+  const network = [mainnet, sepolia, hardhat].find(
+    (chain) => chain.id === config.network.chainId
+  );
   const configVars = config.contracts as Config;
-  const spogClient = new SPOG(rpc, hardhat, configVars);
+  const spogClient = new SPOG(rpc, network!, configVars);
   spogStore.setClient(spogClient);
   return spogClient;
 }
