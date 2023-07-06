fed2badb9719a8bde201e32430d26963f2a4cb13
Luis <hi@ernane.me>
2023-07-06 13:20:01 -0300

    create composables helpers userCOmponentToHtml, useParsedDescriptionTitle

---
+++ composables/useComponentToHtml.ts
@@ -0,0 +1,11 @@
+import { createApp } from "vue";
+
+export const useComponentToHtml = (
+  Component: Component,
+  props: Data | null | undefined
+) => {
+  const component = document.createElement("div");
+  createApp(Component, props).mount(component);
+
+  return { html: component.innerHTML };
+};
