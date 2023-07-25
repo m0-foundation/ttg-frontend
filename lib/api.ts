d9d20227b1cffd350c83b814e1f1f5a589f64215
Luis <hi@ernane.me>
2023-07-25 20:59:45 -0300

    create api function getLists from events + computed final state

--- lib/api.ts
+++ lib/api.ts
@@ -14,7 +14,12 @@ import {
   Log,
   parseAbiItem,
   PublicClient,
+  trim,
 } from "viem";
+
+import orderBy from "lodash/orderBy";
+import groupBy from "lodash/groupBy";
+import forEach from "lodash/forEach";
 
 import {
   ispogGovernorABI,
@@ -26,11 +31,11 @@ import {
 
 export interface EventLog {
   eventName: string;
-  calldatas: Array<string>;
-  endBlock: number;
-  startBlock: number;
-  signatures: Array<string>;
-  targets: Array<string>;
+  calldatas?: Array<string>;
+  endBlock?: number;
+  startBlock?: number;
+  signatures?: Array<string>;
+  targets?: Array<string>;
   blockNumber: number;
   transactionHash: string;
 }
@@ -58,6 +63,17 @@ export interface MProposal extends EventLog {
   description: string;
   state?: keyof typeof ProposalState;
   timestamp: number;
+}
+
+export interface MListEvent extends EventLog {
+  eventName: "AddressAddedToList" | "AddressRemovedFromList";
+  listName: string;
+  account: string;
+  timestamp: number;
+}
+
+export interface MLists {
+  [listName: string]: Array<MListEvent>;
 }
 
 export interface ProposalVotesState {
@@ -622,5 +638,99 @@ export class SPOG {
     };
 
     return values;
+  }
+
+  toString(array: Array<any>) {
+    return array.length > 0 ? array.map((v) => v.toString()) : [];
+  }
+
+  async decodeListLog(log: Log, abi: Abi): Promise<MListEvent> {
+    interface MListDecoded {
+      eventName: string;
+      args: { listName: Hash; account: Hash };
+    }
+
+    const { eventName, args: event } = decodeEventLog({
+      abi,
+      data: log?.data,
+      topics: log?.topics,
+    }) as MListDecoded;
+
+    if (event) {
+      const block = await this.client.getBlock({
+        blockNumber: log.blockNumber!,
+      });
+
+      const listEvent: MListEvent = {
+        ...event,
+        eventName: eventName as "AddressAddedToList" | "AddressRemovedFromList",
+        blockNumber: Number(log.blockNumber),
+        transactionHash: String(log.transactionHash),
+        listName: fromHex(trim(event.listName, { dir: "right" }), "string"),
+        account: event.account,
+        timestamp: Number(block.timestamp),
+      };
+
+      return listEvent;
+    }
+
+    return {} as MListEvent;
+  }
+
+  async getLists(): Promise<Array<MLists>> {
+    const deployedBlock = BigInt(this.config.deployedBlock.toString());
+
+    const addRawLogs = await this.client.getLogs({
+      address: this.config.spog as Hash,
+      fromBlock: deployedBlock,
+      event: parseAbiItem(
+        "event AddressAddedToList(bytes32 listName, address account)"
+      ),
+    });
+
+    const removeRawLogs = await this.client.getLogs({
+      address: this.config.spog as Hash,
+      fromBlock: deployedBlock,
+      event: parseAbiItem(
+        "event AddressRemovedFromList(bytes32 listName, address account)"
+      ),
+    });
+
+    const addOperations = await Promise.all(
+      addRawLogs.map((log: Log) => this.decodeListLog(log, ispogABI))
+    );
+
+    const removeOperations = await Promise.all(
+      removeRawLogs.map((log: Log) => this.decodeListLog(log, ispogABI))
+    );
+
+    const listGrouped = groupBy(
+      orderBy(
+        [...addOperations, ...removeOperations],
+        ["blockNumber"],
+        ["asc"]
+      ),
+      "listName"
+    );
+
+    const lists: MLists[] = [];
+
+    forEach(listGrouped, (events, key) => {
+      let list = new Array<MListEvent>();
+
+      events.forEach((event: MListEvent) => {
+        if (event.eventName === "AddressAddedToList") {
+          list.push(event);
+        }
+
+        if (event.eventName === "AddressRemovedFromList") {
+          list = list.filter((e) => e.account !== event.account);
+        }
+      });
+
+      lists.push({ [key]: list });
+    });
+
+    return lists;
   }
 }
