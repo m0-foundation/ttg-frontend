f0555b59945732b3456d7c004f4bc842a1058061
Luis <hi@ernane.me>
2023-08-08 14:59:30 -0300

    update emergency page add cast vote on button yes/no

--- pages/proposals/emergency.vue
+++ pages/proposals/emergency.vue
@@ -11,7 +11,7 @@
 
       <div v-if="!hasProposals">No Emergency proposals.</div>
       <div v-for="proposal in proposals" v-else :key="proposal.proposalId">
-        <ProposalCard :proposal="proposal" />
+        <ProposalCard :proposal="proposal" @on-cast="onCast" />
       </div>
     </LayoutPage>
   </div>
@@ -19,12 +19,16 @@
 
 <script setup lang="ts">
 import { storeToRefs } from "pinia";
+import { useAccount } from "use-wagmi";
+import { Hash } from "viem";
+import { writeIspogGovernor } from "@/lib/sdk";
 
 const proposalsStore = useProposalsStore();
-const spogStateStore = useSpogStore();
+const spog = useSpogStore();
+const { address: userAccount } = useAccount();
 
 const { getProposalsTypeEmergency: proposals } = storeToRefs(proposalsStore);
-const { epoch } = storeToRefs(spogStateStore);
+const { epoch } = storeToRefs(spog);
 
 const hasProposals = computed(() => {
   return proposals.value && proposals.value.length > 0;
@@ -44,4 +48,14 @@ const timeLeft = computed(() => {
   const { timeAgo } = useDate(Number(epoch.value.next?.asTimestamp));
   return timeAgo;
 });
+
+function onCast(vote: number, proposalId: string) {
+  console.log("cast", { vote, proposalId });
+  return writeIspogGovernor({
+    address: spog.contracts.governor as Hash,
+    functionName: "castVote",
+    args: [BigInt(proposalId), vote],
+    account: userAccount.value,
+  });
+}
 </script>
