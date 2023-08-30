f186dc09a7ee3737582e169b1a90f9af2c21d482
Ernane Aguiar <hi@ernane.me>
2023-08-30 15:04:12 +0200

    rollback

--- middleware/routes.global.ts
+++ middleware/routes.global.ts
@@ -3,4 +3,8 @@ export default defineNuxtRouteMiddleware((to, from) => {
   if (to.path === "/") {
     return navigateTo("/proposals/active");
   }
+
+  if (to.path === "/proposals") {
+    return navigateTo("/proposals/active");
+  }
 });
