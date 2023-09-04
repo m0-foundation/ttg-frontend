2e5a4919079051e4e4cc43cabe0987ba114f2e32
Ernane Aguiar <hi@ernane.me>
2023-09-04 13:53:52 +0200

    add storybook to repo, create firs story: MButton

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./src/**/*.{js,ts,vue}", "./stories/**/*.{js,ts,vue}"],
+  plugins: [],
+  theme: {
+    letterSpacing: {
+      widest: "1.0em",
+    },
+    extend: {
+      colors: {
+        primary: "#00CC9B",
+        "primary-dark": "#009974",
+        "primary-darker": "#00664e",
+        "secondary-dark": "#20221F",
+        "secondary-light": "#FFFFFF",
+        "body-dark": "#0B0B0B",
+        red: "#FF3333",
+        "grey-primary": "#868886",
+        "grey-secondary": "#353835",
+        "grey-800": "#202220",
+        "grey-600": "#363835",
+      },
+    },
+  },
+};
