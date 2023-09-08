c69db1fcd84310b620a2fe36b6d8adc5bac791ec
Ernane Aguiar <hi@ernane.me>
2023-09-08 13:43:24 +0200

    fix bug by add zindex to not overlap with other elements

--- components/design-system/MInputMultiSelect.vue
+++ components/design-system/MInputMultiSelect.vue
@@ -16,7 +16,10 @@
 
       <span v-else class="text-xs text-[#5d605d]">change</span>
     </button>
-    <ul v-show="isMenuOpen" class="absolute text-white pt-4 bg-grey-secondary">
+    <ul
+      v-show="isMenuOpen"
+      class="absolute z-50 text-white pt-4 bg-grey-secondary"
+    >
       <li v-for="opt in options" :key="opt.value" class="">
         <div
           v-show="opt.header"
