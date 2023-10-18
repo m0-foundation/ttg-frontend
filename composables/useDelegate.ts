e5621dbea39b4bbb09f7edda4d6032743f675bab
Ernane Aguiar <hi@ernane.me>
2023-10-18 19:08:08 -0300

    create composables for delegates and add new type to epoch

---
+++ composables/useDelegate.ts
@@ -0,0 +1,27 @@
+import { useAccount } from "use-wagmi";
+import { useMDelegates } from "@/lib/hooks";
+
+export default () => {
+  const { address: userAccount } = useAccount();
+  const { powerDelegates, zeroDelegates } = useMDelegates(userAccount);
+
+  // has Delegated if the connected wallet is not the same as the delegates
+  const hasDelegatedPower = computed(
+    () =>
+      powerDelegates?.data?.value?.toLowerCase() !==
+      userAccount.value?.toLowerCase()
+  );
+
+  const hasDelegatedZero = computed(
+    () =>
+      zeroDelegates?.data?.value?.toLowerCase() !==
+      userAccount.value?.toLowerCase()
+  );
+
+  return {
+    powerDelegates: powerDelegates?.data?.value,
+    zeroDelegates: zeroDelegates?.data?.value,
+    hasDelegatedPower,
+    hasDelegatedZero,
+  };
+};
