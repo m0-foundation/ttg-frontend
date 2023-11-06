9b1f58f09a8115d901ecb82a46aae9c51a7846c5
Ernane Aguiar <hi@ernane.me>
2023-11-06 18:01:00 -0300

    fix color

--- components/proposal/TableVotes.vue
+++ components/proposal/TableVotes.vue
@@ -49,7 +49,7 @@ const config = computed(() => {
         sort: true,
         formatter: (cell: boolean, row: any) => {
           const vote = cell
-            ? "<span class='bg-green-700 text-body-black px-2 py-1'>YES</span>"
+            ? "<span class='bg-green-700 text-white px-2 py-1'>YES</span>"
             : "<span class='bg-red-500 text-white px-2 py-1'>NO</span>";
 
           const transactionHash = row.cells[3].data;
