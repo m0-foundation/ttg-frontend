e5ea8b1c8d0cf2363c473daeac8270645581e1c8
Ernane Aguiar <hi@ernane.me>
2023-11-06 17:54:46 -0300

    create composable that look at current connected chain and compare to the one it should be connected, if differ switch it.

---
+++ composables/useCorrectChain.ts
@@ -0,0 +1,28 @@
+import { useSwitchNetwork, useNetwork, Chain } from "use-wagmi";
+
+export default () => {
+  const network = useNetworkStore().getNetwork();
+  const chainId = computed(() => network.value.rpc.chainId);
+
+  const { chain: connectedChain } = useNetwork();
+  const { switchNetworkAsync } = useSwitchNetwork();
+
+  function forceSwitchChain() {
+    console.log({
+      connectedChain: connectedChain?.value,
+      chainId: chainId.value,
+    });
+
+    if (
+      connectedChain &&
+      connectedChain.value &&
+      connectedChain.value.id !== chainId.value
+    ) {
+      return switchNetworkAsync(chainId.value);
+    } else {
+      return Promise.resolve(connectedChain?.value as Chain);
+    }
+  }
+
+  return { forceSwitchChain };
+};
