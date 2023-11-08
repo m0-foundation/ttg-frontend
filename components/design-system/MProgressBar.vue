50a081a1908f353fd8ce16b380d96828f8c25464
Ernane Aguiar <hi@ernane.me>
2023-11-08 11:08:54 -0300

    Refactor mprogressbar remove quorom version

--- components/design-system/MProgressBar.vue
+++ components/design-system/MProgressBar.vue
@@ -1,17 +1,7 @@
 <template>
-  <div
-    :class="[
-      'w-full',
-      'h-2',
-      {
-        'bg-red-500': version === 'majority',
-        'bg-gray-200': version === 'quorum',
-      },
-    ]"
-  >
+  <div class="w-full h-2 bg-red-500">
     <div>
-      <div class="bg-green-700 h-2" :style="`width: ${width}%`"></div>
-      <div v-if="version === 'quorum'" class="quorum" :style="style" />
+      <div class="bg-green-700 h-2" :style="`width: ${width}%`" />
     </div>
   </div>
 </template>
@@ -19,25 +9,9 @@
 <script lang="ts" setup>
 export interface MProgressBarProps {
   width: number;
-  version: "majority" | "quorum";
-  quorum?: number;
 }
 
 const props = withDefaults(defineProps<MProgressBarProps>(), {
   width: 0,
-  version: "majority",
-  quorum: 0.5,
-});
-
-const style = computed(() => {
-  return {
-    left: `${props.quorum * 100}%`,
-  };
 });
 </script>
-
-<style scoped>
-.quorum {
-  @apply relative z-10 border-r-2 border-gray-400 w-2 h-6 top-[-1rem];
-}
-</style>
