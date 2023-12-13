e4be07bad8641ff0e691345341aae8054210b491
Conrado Cañas <146194347+conradocanasm0@users.noreply.github.com>
2023-12-13 12:47:46 -0300

    WEB3 547 Fix Copy to Clipboard function (#113)
    * WEB3 547 Fix Copy to Clipboard function
    
    * Update components/proposal/ProposalMenu.vue
    
    Co-authored-by: Ernane Luis  <hi@ernane.me>
    
    ---------
    
    Co-authored-by: Ernane Luis <hi@ernane.me>

--- utils/misc.ts
+++ utils/misc.ts
@@ -1,4 +1,14 @@
 export const copyToClipboard = (text: undefined | string) => {
   if (!text) return;
-  navigator?.clipboard?.writeText(text);
+  if (navigator?.clipboard) {
+    return navigator.clipboard.writeText(text);
+  } else {
+    const textarea = document.createElement("textarea");
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+    return Promise.resolve();
+  }
 };
