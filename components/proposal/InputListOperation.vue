e0a6333f4b7266101d3da2d5955988d6234983a9
Ernane Aguiar <hi@ernane.me>
2023-12-15 11:56:57 -0300

    remove not used code

--- components/proposal/InputListOperation.vue
+++ components/proposal/InputListOperation.vue
@@ -20,8 +20,6 @@
 <script setup lang="ts">
 import { ErrorObject } from "@vuelidate/core";
 
-const options = ["minters", "validators", "earners"];
-
 export interface InputProps {
   modelValue: string;
   modelValue2: string;
@@ -34,17 +32,6 @@ const emit = defineEmits(["update:modelValue", "update:modelValue2"]);
 
 const list = useVModelWrapper<InputProps>(props, emit, "modelValue");
 const address = useVModelWrapper<InputProps>(props, emit, "modelValue2");
-
-const hasErrors = computed(() => props.modelValueErrors?.length);
-
-const isOtherList = ref(false);
-
-function handleChangeList(e: any) {
-  if (e.target.value === "otherList") {
-    list.value = "";
-    isOtherList.value = true;
-  }
-}
 </script>
 
 <style>
