9269862d67fe67023744c6484488dafcd79a338f
Ernane Aguiar <hi@ernane.me>
2024-01-03 12:55:10 -0300

    fix warning of marked lib by adding new missing libs

--- composables/useParsedDescription.ts
+++ composables/useParsedDescription.ts
@@ -1,5 +1,14 @@
 import { marked } from "marked";
+import { gfmHeadingId } from "marked-gfm-heading-id";
+import { mangle } from "marked-mangle";
 import xss from "xss";
+
+const options = {
+  prefix: "header-",
+};
+
+marked.use(gfmHeadingId(options));
+marked.use(mangle());
 
 export const useParsedDescription = (description: string) => {
   const html = xss(marked.parse(description));
