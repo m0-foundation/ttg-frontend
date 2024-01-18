a0b7e6ffa0535bc70f909871677cb1caac257d62
Conrado Canas <conrado.canas@m0.xyz>
2024-01-18 00:55:55 -0300

    Proposals showcase test to have all proposals on-screen

---
+++ cypress/e2e/proposals-showcase.cy.ts
@@ -0,0 +1,162 @@
+describe("Proposals", () => {
+  const proposalUrl = "";
+
+  describe("Create showcase proposals", () => {
+    const input1 = "minters";
+    const input2 = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+    const description = `Add ${input2} to list ${input1}`;
+
+    const titleEmergency = "Emergency proposal";
+    const titlePending = "Pending proposal";
+    const titleStandard = "Standard proposal";
+    const titleReset = "Zero proposal";
+    const titleExecutable = "Executable proposal";
+    const descriptionTest = "test proposal description";
+
+    it("Standard proposal add to list", () => {
+      cy.visit("http://localhost:3000/proposal/create");
+      cy.connectWallet();
+
+      cy.get("[data-test='proposalTypeSelect']").should("exist").click();
+
+      cy.get("[data-test='addToList']").click();
+
+      cy.get("[data-test='listSelect']").click();
+      cy.get(`[data-test='list_${input1}']`).click();
+
+      // address to append
+      cy.get("input[data-test='proposalValue2']").type(input2);
+      cy.get("input[data-test='title']").type(titleStandard);
+
+      cy.get("textarea[data-test='description']").type(descriptionTest);
+
+      cy.clickPreviewProposal();
+
+      cy.contains("Submit proposal").should("exist");
+      cy.contains("Submit proposal").then(($el) => {
+        $el.click();
+        cy.get(".complete").should("have.length", 3);
+      });
+    });
+
+    it("Vote standard proposal to show in active proposals", () => {
+      // forward in time to be able to vote
+      cy.mineEpochs(2);
+
+      cy.wait(500);
+    });
+
+    it("Zero proposal reset", () => {
+      // zero proposals cant be created on first epoch
+
+      cy.visit("http://localhost:3000/proposal/create");
+      cy.connectWallet();
+
+      cy.get("[data-test='proposalTypeSelect']").should("exist").click();
+
+      cy.get("[data-test='menuReset']").click();
+      cy.get("[data-test='resetToPowerHolders']").click();
+
+      cy.get("input[data-test='proposalValue']").should("not.exist");
+
+      cy.get("input[data-test='title']").type(titleReset);
+      cy.get("textarea[data-test='description']").type(descriptionTest);
+
+      cy.clickPreviewProposal();
+
+      cy.contains("Submit proposal").should("exist");
+      cy.contains("Submit proposal").then(($el) => {
+        $el.click();
+        cy.get(".complete").should("have.length", 3);
+      });
+    });
+
+    it("Standard proposal add to list for pending", () => {
+      cy.visit("http://localhost:3000/proposal/create");
+      cy.connectWallet();
+
+      cy.get("[data-test='proposalTypeSelect']").should("exist").click();
+
+      cy.get("[data-test='addToList']").click();
+
+      cy.get("[data-test='listSelect']").click();
+      cy.get(`[data-test='list_${input1}']`).click();
+
+      // address to append
+      cy.get("input[data-test='proposalValue2']").type(input2);
+      cy.get("input[data-test='title']").type(titlePending);
+
+      cy.get("textarea[data-test='description']").type(descriptionTest);
+
+      cy.clickPreviewProposal();
+
+      cy.contains("Submit proposal").should("exist");
+      cy.contains("Submit proposal").then(($el) => {
+        $el.click();
+        cy.get(".complete").should("have.length", 3);
+      });
+    });
+
+    it("Emergency proposal add to list", () => {
+      cy.visit("http://localhost:3000/proposal/create");
+      cy.connectWallet();
+
+      cy.get("[data-test='proposalTypeSelect']").should("exist").click();
+
+      cy.get("[data-test='menuEmergency']").click();
+      cy.get("[data-test='emergencyAddToList']").click({ force: true });
+
+      cy.get("[data-test='listSelect']").click();
+      cy.get(`[data-test='list_${input1}']`).click();
+
+      cy.get("input[data-test='proposalValue2']").type(input2);
+      cy.get("input[data-test='title']").type(titleExecutable);
+      cy.get("textarea[data-test='description']").type(descriptionTest);
+
+      cy.clickPreviewProposal();
+
+      cy.contains("Submit proposal").should("exist");
+      cy.contains("Submit proposal").then(($el) => {
+        $el.click();
+        cy.get(".complete").should("have.length", 3);
+      });
+    });
+
+    it("Vote YES to proposal to be executable", () => {
+      cy.visit("http://localhost:3000/proposals/emergency");
+      cy.connectWallet();
+      cy.wait(500);
+
+      cy.contains("article", descriptionTest).then(($proposal) => {
+        cy.wrap($proposal).find("#button-cast-yes").click();
+      });
+
+      cy.reload();
+    });
+
+    it("Emergency proposal add to list", () => {
+      cy.visit("http://localhost:3000/proposal/create");
+      cy.connectWallet();
+
+      cy.get("[data-test='proposalTypeSelect']").should("exist").click();
+
+      cy.get("[data-test='menuEmergency']").click();
+      cy.get("[data-test='emergencyAddToList']").click({ force: true });
+
+      cy.get("[data-test='listSelect']").click();
+      cy.get("[data-test='list_validators']").click();
+
+      cy.get("input[data-test='proposalValue2']").type(input2);
+      cy.get("input[data-test='title']").type(titleEmergency);
+      cy.get("textarea[data-test='description']").type(descriptionTest);
+
+      cy.clickPreviewProposal();
+
+      cy.contains("Submit proposal").should("exist");
+      cy.contains("Submit proposal").then(($el) => {
+        $el.click();
+        cy.get(".complete").should("have.length", 3);
+      });
+    });
+  });
+});
