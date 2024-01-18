677ce1c3ea142d8c1fbcc69b9e26d6dccfb5069a
Ernane Aguiar <hi@ernane.me>
2024-01-18 20:56:11 -0300

    refactor inputs for each type of proposal that needs validation and mask

--- components/proposal/InputGovernanceConfigOperation.vue
+++ components/proposal/InputGovernanceConfigOperation.vue
@@ -3,18 +3,21 @@
     <SelectGovernanceConfig
       v-model="key"
       class="mb-6"
-      :errors="props.modelValueErrors"
+      @handle-change-list="handleChangeList"
     />
 
     <div>
       <label for="governance-config-input">Value *</label>
-      <MInput
+
+      <component
+        :is="inputs[selectedInput].component"
+        v-if="inputs[selectedInput]"
+        v-bind="inputs[selectedInput].props"
         id="governance-config-input"
-        v-model="value"
-        class="input"
-        type="text"
-        data-test="proposalValue2"
-        :errors="props.modelValue2Errors"
+        v-model="value1"
+        v-model:modelValue2="value2"
+        :model-value-errors="props.modelValueErrors"
+        :model-value2-errors="props.modelValue2Errors"
       />
     </div>
   </div>
@@ -22,7 +25,10 @@
 
 <script setup lang="ts">
 import { ErrorObject } from "@vuelidate/core";
-import { MVotingTokens } from "@/lib/api";
+import InputDynamic from "./InputDynamic.vue";
+import InputDynamicDoubleModel from "./InputDynamicDoubleModel.vue";
+
+import { masks } from "@/utils/masks";
 
 export interface InputProps {
   modelValue: string;
@@ -34,8 +40,57 @@ export interface InputProps {
 const props = defineProps<InputProps>();
 const emit = defineEmits(["update:modelValue", "update:modelValue2"]);
 
-const key = useVModelWrapper<InputProps>(props, emit, "modelValue");
-const value = useVModelWrapper<InputProps>(props, emit, "modelValue2");
+const key = ref();
+
+const value1 = useVModelWrapper<InputProps>(props, emit, "modelValue");
+const value2 = useVModelWrapper<InputProps>(props, emit, "modelValue2");
+
+const inputs = {
+  setProposalFee: {
+    component: InputDynamic,
+    props: {
+      decorator: "WETH",
+      maska: masks.eth,
+    },
+  },
+
+  setCashToken: {
+    component: InputDynamicDoubleModel,
+    props: {
+      input1: {
+        decorator: "Address",
+        maska: masks.ethereumAddress,
+      },
+      input2: {
+        decorator: "WETH",
+        maska: masks.eth,
+      },
+    },
+  },
+
+  setZeroProposalThresholdRatio: {
+    component: InputDynamic,
+    props: {
+      decorator: "%",
+      maska: masks.percentage,
+    },
+  },
+  setEmergencyProposalThresholdRatio: {
+    component: InputDynamic,
+    props: {
+      decorator: "%",
+      maska: masks.percentage,
+    },
+  },
+};
+
+const selectedInput = computed(() => key.value);
+
+function handleChangeList() {
+  console.log("handleChangeList");
+  value1.value = undefined;
+  value2.value = undefined;
+}
 </script>
 
 <style>
