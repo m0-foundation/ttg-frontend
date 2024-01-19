5bdedfddbaf8c0b131301f34363aeb963d99a1cd
Conrado Canas <conrado.canas@m0.xyz>
2024-01-19 00:20:31 -0300

    All proposals page update for new design

--- components/page/PageTitle.vue
+++ components/page/PageTitle.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="p-6 pt-2 lg:pt-6 w-full lg:flex justify-between">
+  <div class="p-8 py-2 lg:pt-6 w-full lg:flex justify-between items-end">
     <div>
+      <NuxtLink
+        class="text-green-700 hover:text-green-600 text-sm uppercase cursor-pointer"
+        @click="router.back()"
+        >Back</NuxtLink
+      >
       <h1
         class="text-white text-lg font-normal font-['IBM Plex Mono'] uppercase"
       >
@@ -12,3 +17,7 @@
     <slot name="side"></slot>
   </div>
 </template>
+
+<script setup>
+const router = useRouter();
+</script>
