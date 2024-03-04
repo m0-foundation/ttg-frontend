9d68a82335121babd29a1f7a938a10565bb9c0ae
Ernane Aguiar <hi@ernane.me>
2024-03-04 19:46:10 -0300

    refactor useMdelegate to query by input address

--- lib/hooks/useMDelegatesZero.ts
+++ lib/hooks/useMDelegatesZero.ts
@@ -1,11 +1,17 @@
 import { storeToRefs } from "pinia";
 import { Hash } from "viem";
-import { useReadContract, useAccount } from "use-wagmi";
+import { useReadContract } from "use-wagmi";
 import { zeroTokenAbi } from "@/lib/sdk";
 import { useSpogStore } from "@/stores/spog";
 
-export default () => {
-  const { address, isConnected } = useAccount();
+export default (
+  userAccount:
+    | globalThis.Ref<undefined>
+    | globalThis.Ref<`0x${string}`>
+    | globalThis.Ref<`0x${string}` | undefined>
+) => {
+  // keep the reactive from the prop alive
+  const account = ref(userAccount);
   const store = useSpogStore();
   const spog = storeToRefs(store);
 
@@ -13,10 +19,8 @@ export default () => {
     address: spog.contracts.value.zeroToken as Hash,
     abi: zeroTokenAbi,
     functionName: "delegates",
-    args: [address as Ref<Hash>],
-    //     watch: true,
+    args: [account as Ref<Hash>],
     query: {
-      enabled: isConnected,
       select: (data) => {
         console.log("delegates", data);
         return String(data);
