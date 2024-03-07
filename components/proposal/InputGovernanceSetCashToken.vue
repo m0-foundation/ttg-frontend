ee4319b00bbf5e2f8464d2b6949cebf1b36f1653
Ernane Aguiar <hi@ernane.me>
2024-03-07 14:41:05 -0300

    refactor setCashToken input to be a selector if allowedCashTokens config data is exists otherwise show text input

--- components/proposal/InputGovernanceSetCashToken.vue
+++ components/proposal/InputGovernanceSetCashToken.vue
@@ -1,24 +1,61 @@
 <template>
-  <div class="w-full flex justify-between items-center space-x-4">
+  <div class="w-full flex justify-between space-x-4">
     <div class="block w-1/2">
-      <InputDynamic
-        v-model="address"
-        :model-value-errors="modelValueErrors"
-        :maska="masks.ethereumAddress"
-        description="Update the currency used for payment when submitting proposal"
-        placeholder="Token Address"
-        data-test="proposalValue"
-      />
+      <div
+        v-if="hasAllowedCashTokens"
+        class="bg-accent-teal flex flex-col gap-1 p-4 my-2"
+      >
+        <span class="uppercase text-xxs">Parameter description</span>
+        <p class="font-inter">
+          Update the currency used for payment when submitting proposal
+        </p>
+      </div>
+
+      <div>
+        <div v-if="hasAllowedCashTokens">
+          <label for="governance-config-input">Value *</label>
+          <MInputMultiSelect
+            v-model="address"
+            label="Select Cash token"
+            data-test="select-cash-token"
+            :options="options"
+            :class="{ error: hasErrors }"
+            description="Update the currency used for payment when submitting proposal"
+            @on-change="handleChangeList"
+          />
+          <span class="text-xxs">
+            {{ address }}
+          </span>
+        </div>
+        <div v-else>
+          <InputDynamic
+            v-model="address"
+            :model-value-errors="modelValueErrors"
+            :maska="masks.ethereumAddress"
+            description="Update the currency used for payment when submitting proposal"
+            placeholder="Token Address"
+            data-test="proposalValue"
+          />
+        </div>
+
+        <div class="text-red-500 text-xs my-2 h-4">
+          <p v-for="error of props.modelValueErrors" :key="error.$uid">
+            {{ error.$message }}
+          </p>
+        </div>
+      </div>
     </div>
 
     <div class="block w-1/2">
       <InputDynamic
-        v-model="fee"
+        v-model="inputTaxAmount"
         :model-value-errors="modelValue2Errors"
         :maska="masks.eth"
-        description="Update the fee charged for submitting proposals based on the new Token."
-        placeholder="New Proposal Fee"
+        description="Update the Tax charged for submitting proposals based on the new Token."
+        placeholder="Amount"
         data-test="proposalValue2"
+        :decorator="selectedToken?.symbol"
+        :disabled="hasAllowedCashTokens && !selectedToken"
       />
     </div>
   </div>
@@ -26,6 +63,7 @@
 
 <script setup lang="ts">
 import { ErrorObject } from "@vuelidate/core";
+import { parseUnits, Hash } from "viem";
 import { masks } from "@/utils/masks";
 
 export interface InputProps {
@@ -35,11 +73,51 @@ export interface InputProps {
   modelValue2Errors?: ErrorObject[];
 }
 
+interface TokenOptionItem {
+  value: Hash;
+  label?: string;
+  id?: string;
+  decimals?: number;
+  symbol?: string;
+}
+
 const props = defineProps<InputProps>();
 const emit = defineEmits(["update:modelValue", "update:modelValue2"]);
 
 const address = useVModelWrapper<InputProps>(props, emit, "modelValue");
-const fee = useVModelWrapper<InputProps>(props, emit, "modelValue2");
+const tax = useVModelWrapper<InputProps>(props, emit, "modelValue2"); // in bigint wei already with the selected token's decimals
+const inputTaxAmount = ref();
+
+const spogStore = useSpogStore();
+const hasErrors = computed(() => props.modelValueErrors?.length);
+
+const selectedToken = ref<TokenOptionItem>();
+const hasAllowedCashTokens = computed(
+  () => (spogStore.governors.zero.allowedCashTokens?.length ?? 0) > 0
+);
+
+const options: TokenOptionItem[] | undefined =
+  spogStore.governors.zero.allowedCashTokens?.map((token) => ({
+    value: token.address,
+    label: token.symbol,
+    id: token.symbol,
+    decimals: token.decimals,
+    symbol: token.symbol,
+  }));
+
+function handleChangeList(option: any) {
+  address.value = option.value;
+  selectedToken.value = option;
+  inputTaxAmount.value = undefined;
+}
+
+tax.value = computed(() => {
+  if (!inputTaxAmount.value) return undefined;
+  if (hasAllowedCashTokens.value) {
+    return parseUnits(inputTaxAmount.value, selectedToken.value?.decimals || 0);
+  }
+  return inputTaxAmount.value;
+});
 </script>
 
 <style scoped>
