1cc2242f9c59d13fe7a2922663ff9bfb9f14c2ca
Ernane Aguiar <hi@ernane.me>
2024-03-23 15:39:04 -0300

    fix hardhat to support new module ttg

--- network/local.config.ts
+++ network/local.config.ts
@@ -1,10 +1,19 @@
 import { NetworkConfig } from "./types";
 
 export default {
+  network: "hardhat",
   contracts: {
     registrar: "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6",
     multicall3: "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318",
     deploymentBlock: 0,
+    zero: {
+      allowedCashTokens: [
+        "0x5FbDB2315678afecb367f032d93F642f64180aa3", // WETH,
+
+        "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+        // "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", // M token
+      ],
+    },
   },
   rpc: {
     chainId: 31337,
