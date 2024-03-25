d97953d08ee9e50cb11b58daac09d85f7e73209d
Conrado Canas <conrado.canas@m0.xyz>
2024-03-25 17:25:09 -0300

    Refactor rewards page layout and functionality with contract

--- pages/rewards.vue
+++ pages/rewards.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <PageTitle class="mb-8 px-6 lg:p-0">
+    <PageTitle class="px-6 lg:p-0 mb-8">
       <template #default>Rewards</template>
       <template #subtitle>
         <p class="text-grey-500 font-inter">
@@ -11,70 +11,55 @@
             target="_blank"
             rel="noopener noreferrer"
             class="underline"
-            >Learn more </a
+            >Learn more</a
           >.
         </p>
       </template>
     </PageTitle>
 
-    <div class="px-6 lg:p-0">
+    <div class="px-6 lg:p-0 my-8">
       <h3 class="text-sm font-inter text-grey-200 mb-3">Total amount</h3>
-      <div class="flex gap-8">
-        <div>
-          <span class="token-label">M Token</span>
+      <div class="flex flex-wrap gap-4 lg:gap-8">
+        <div v-for="token in cashTokens" :key="token.address">
+          <span class="token-label">{{ token.name }}</span>
           <MTokenAmount
-            :amount="totalClaimableM"
-            image="/img/tokens/m.svg"
-            name="M"
-            size="20"
-          />
-        </div>
-        <div>
-          <span class="token-label">WETH Token</span>
-          <MTokenAmount
-            :amount="totalClaimableWeth"
-            image="/img/tokens/weth10.png"
-            name="WETH"
+            :amount="formatUnits(token.vaultBalance, token.decimals)"
+            :image="`/img/tokens/${token.symbol.toLowerCase()}.svg`"
+            :name="token.name"
             size="20"
           />
         </div>
       </div>
     </div>
 
-    <div class="mt-8 px-6 lg:p-0">
+    <div class="px-6 lg:p-0 my-12">
       <h3 class="text-sm font-inter text-grey-200 mb-3">
         Claimable as ZERO holder
       </h3>
 
       <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
-        <div class="bg-grey-800 p-6">
+        <div
+          v-for="(token, i) in cashTokens"
+          :key="token.address"
+          class="bg-grey-800 p-6"
+        >
           <div class="flex justify-between items-end">
             <div>
-              <h4 class="mb-3">M Token</h4>
+              <h4 class="mb-3">{{ token.name }}</h4>
               <span class="token-label">Claimable balance</span>
               <MTokenAmount
-                :amount="userClaimableM"
-                image="/img/tokens/m.svg"
-                name="M"
+                :amount="token.claimable"
+                :image="`/img/tokens/${token.symbol.toLowerCase()}.svg`"
+                :name="token.name"
                 size="30"
               />
             </div>
-            <MButton :disabled="!userClaimableM">Claim</MButton>
-          </div>
-        </div>
-        <div class="bg-grey-800 p-6">
-          <div class="flex justify-between items-end">
-            <div>
-              <h4 class="mb-3">WETH Token</h4>
-              <span class="token-label">Claimable balance</span>
-              <MTokenAmount
-                :amount="userClaimableWeth"
-                image="/img/tokens/weth10.png"
-                name="WETH"
-                size="30"
-              />
-            </div>
-            <MButton :disabled="!userClaimableWeth">Claim</MButton>
+            <MButton
+              :disabled="token.claimable === 0n || token.isClaiming"
+              :is-loading="token.isClaiming"
+              @click="claimTokenRewards(token, i)"
+              >Claim</MButton
+            >
           </div>
         </div>
       </div>
@@ -86,12 +71,114 @@
   </div>
 </template>
 
-<script setup>
-const userClaimableWeth = ref(0);
-const userClaimableM = ref(0);
+<script setup lang="ts">
+import { Hash, erc20Abi, formatUnits } from "viem";
+import {
+  readContract,
+  writeContract,
+  waitForTransactionReceipt,
+} from "@wagmi/core";
+import { useAccount } from "use-wagmi";
+import { distributionVaultAbi } from "@/lib/sdk";
 
-const totalClaimableWeth = ref(0);
-const totalClaimableM = ref(0);
+useHead({
+  titleTemplate: "%s - Rewards",
+});
+
+const cashTokens = ref([]);
+
+const { epoch } = storeToRefs(useSpogStore());
+const { address: userAccount } = useAccount();
+
+const wagmiConfig = useWagmiConfig();
+const spog = useSpogStore();
+const alerts = useAlertsStore();
+
+const allowedCashTokens = computed(() => spog.governors.zero.allowedCashTokens);
+
+const claimEpochStart = computed(() =>
+  BigInt(epoch.value.current.asNumber - 50)
+);
+const claimEpochEnd = computed(() => BigInt(epoch.value.current.asNumber - 1));
+
+onMounted(async () => {
+  cashTokens.value = await Promise.all(
+    allowedCashTokens.value.map(async (token) => {
+      return {
+        ...token,
+        claimable: await getClaimableRewards(token),
+        vaultBalance: await getVaultTokenBalance(token),
+      };
+    })
+  );
+});
+
+const getVaultTokenBalance = async (token) => {
+  try {
+    return await readContract(wagmiConfig, {
+      abi: erc20Abi,
+      address: token.address as Hash,
+      functionName: "balanceOf",
+      args: [spog.contracts.vault as Hash], // address account
+    });
+  } catch (error) {
+    return 0n;
+  }
+};
+
+const getClaimableRewards = async (token) => {
+  try {
+    return await readContract(wagmiConfig, {
+      abi: distributionVaultAbi,
+      address: spog.contracts.vault as Hash,
+      functionName: "getClaimable",
+      args: [
+        token.address as Hash,
+        userAccount.value as Hash,
+        claimEpochStart.value,
+        claimEpochEnd.value,
+      ], // address token, address account, uint256 startEpoch, uint256 endEpoch
+    });
+  } catch (error) {
+    console.log("ERROR GETTING CLAIMABLE", error);
+    return 0n;
+  }
+};
+
+const claimTokenRewards = async (token, index) => {
+  cashTokens.value[index].isClaiming = true;
+
+  try {
+    const hash = await writeContract(wagmiConfig, {
+      abi: distributionVaultAbi,
+      address: spog.contracts.vault as Hash,
+      functionName: "claim",
+      args: [
+        token.address as Hash,
+        claimEpochStart.value,
+        claimEpochEnd.value,
+        userAccount.value as Hash,
+      ],
+    }); // address token, uint256 startEpoch, uint256 endEpoch, address destination
+
+    const txReceipt = await waitForTransactionReceipt(wagmiConfig, {
+      confirmations: 1,
+      hash,
+    });
+
+    if (txReceipt.status !== "success") {
+      throw new Error("Transaction was rejected");
+    } else {
+      alerts.successAlert(`You claimed your ${token.name} rewards!`);
+    }
+
+    console.log("CLAIMED TOKEN", amount);
+  } catch (error) {
+    console.log("ERROR CLAIMING TOKEN", error);
+  } finally {
+    cashTokens.value[index].isClaiming = false;
+  }
+};
 </script>
 
 <style>
