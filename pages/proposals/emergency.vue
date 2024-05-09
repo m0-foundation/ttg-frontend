7a35ee2c92d2fd882953e94492eee87ed0edf7dd
Ernane Aguiar <hi@ernane.me>
2024-05-09 12:39:13 -0300

    update emergency to use batch voting

--- pages/proposals/emergency.vue
+++ pages/proposals/emergency.vue
@@ -9,20 +9,27 @@
       >
         <template #emptyState>
           <ProposalListEmptyState>
-            No active standard proposals
+            No active emergency proposals
           </ProposalListEmptyState>
         </template>
       </ProposalList>
 
       <div
-        v-show="isConnected"
+        v-show="hasProposals && isConnected"
         class="lg:flex justify-end items-center gap-4 mt-6 py-4 px-8"
+        :class="{
+          'bg-grey-700': isSelectedCastProposalsFull,
+        }"
       >
-        <span class="text-xxs"> Select YES or NO to submit your vote </span>
+        <span v-if="!isSelectedCastProposalsFull" class="text-xxs">
+          Select YES or NO to submit your vote
+        </span>
         <MButton
           id="button-cast-submit"
           class="w-full lg:w-40 flex justify-center"
-          :disabled="isLoading || selectedCastProposals.length === 0"
+          :disabled="
+            !isSelectedCastProposalsFull || hasVotedOnAllProposals || isLoading
+          "
           :is-loading="isLoading"
           data-test="proposal-button-submit-votes"
           @click="onCastBatchVotes"
@@ -59,13 +66,18 @@ const activeProposals = computed(() =>
 const emergencyProposals = computed(() =>
   activeProposals.value.filter((p) => p.votingType === "Emergency")
 );
+const hasProposals = computed(
+  () => emergencyProposals && emergencyProposals.value.length > 0
+);
+
+const isSelectedCastProposalsFull = computed(() => {
+  return selectedCastProposals.value.length === emergencyProposals.value.length;
+});
 
 const { address: userAccount, isConnected } = useAccount();
 const { forceSwitchChain } = useCorrectChain();
 const wagmiConfig = useWagmiConfig();
 const alerts = useAlertsStore();
-
-const balances = useMBalances(userAccount);
 
 useHead({
   titleTemplate: "%s - Proposals",
@@ -76,11 +88,27 @@ function onCast(vote: number, proposalId: string) {
 }
 
 function onUncast(proposalId: string) {
+  console.log("uncast", proposalId, selectedCastProposals.value);
   selectedCastProposals.value = selectedCastProposals.value.filter(
     (p) => p.proposalId !== proposalId
   );
 }
 
+const hasVotedOnAllProposals = ref(false);
+
+// const { data: hasVotedOnAllProposals, ...votedOnAllProposals } =
+//   useReadContract({
+//     address: spog.contracts.standardGovernor as Hash,
+//     abi: standardGovernorAbi,
+//     functionName: "hasVotedOnAllProposals",
+//     args: [
+//       userAccount as Ref<Hash>,
+//       BigInt(spog.epoch?.current?.asNumber || 0),
+//     ],
+//     query: {
+//       enabled: isConnected,
+//     },
+//   });
 // batch is only for standard proposals
 async function onCastBatchVotes() {
   await forceSwitchChain();
@@ -109,8 +137,7 @@ async function onCastBatchVotes() {
       throw txReceipt;
     }
 
-    await spog.fetchTokens();
-    balances.refetch();
+    alerts.successAlert("Vote casted successfully!");
   } catch (error: any) {
     console.log("Error casting vote", { error });
     if (error.transactionHash) {
