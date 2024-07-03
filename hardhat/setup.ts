dd89730597bc13d5fe706d2a0fd1ff7face24a86
Ernane Aguiar <hi@ernane.me>
2024-07-03 20:12:03 -0300

    update epochs values from production

--- hardhat/setup.ts
+++ hardhat/setup.ts
@@ -25,8 +25,8 @@ const PORT = 8545;
 const _BLOCK_TIME = 12;
 
 // see file lib/api/modules/epoch/epoch.ts#L17
-const _STARTING_TIMESTAMP = 1_714_154_183;
-const _EPOCH_PERIOD_SECONDS = 900;
+const _STARTING_TIMESTAMP = 1_713_099_600;
+const _EPOCH_PERIOD_SECONDS = 1_296_000;
 
 type ChainServer = {
   address: string;
