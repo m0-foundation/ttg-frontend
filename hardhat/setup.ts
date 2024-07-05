43eaae89ec2b458921dd6c1e9b5c8d3daa30214d
Ernane Aguiar <hi@ernane.me>
2024-07-04 21:19:36 -0300

    fix mining blocks epoch calculation of timestamp

--- hardhat/setup.ts
+++ hardhat/setup.ts
@@ -180,12 +180,12 @@ export default async function setup(): Promise<
         ...chainServers.map((server) => server.close()),
       ]);
     },
-    mine: async (quantity) => {
-      const blocks = (_EPOCH_PERIOD_SECONDS / _BLOCK_TIME) * quantity;
-
+    mine: async (epoch) => {
+      const blocks = (_EPOCH_PERIOD_SECONDS / _BLOCK_TIME) * epoch;
       const currentTimestamp = await hhHelpers.time.latest();
-      const newTimestamp = currentTimestamp + (blocks-1) * _BLOCK_TIME;
-      console.log({ quantity, currentTimestamp, newTimestamp });
+      const currentEpoch = getEpochFromTimestamp(currentTimestamp);
+      const newTimestamp = getTimestampOfEpochStart(currentEpoch + epoch);
+      console.log({ epoch, currentTimestamp, newTimestamp });
       await hhHelpers.time.setNextBlockTimestamp(newTimestamp);
       await hre.network.provider.send("hardhat_mine", [
         "0x" + blocks.toString(16),
