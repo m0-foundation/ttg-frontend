c9e08de92c9fe2b92dccbd2791f422965871cddd
Conrado Cañas <conrado.canas@m0.xyz>
2025-01-04 14:39:18 -0300

    WEB 1324 Add header and ecosystem menu

---
+++ components/AppHeader.vue
@@ -0,0 +1,50 @@
+<template>
+  <UHeader :links="links">
+    <template #logo>
+      <div class="flex items-center gap-4">
+        <img
+          class="h-[var(--header-height)]"
+          src="~/assets/images/m0_white_black.png"
+        />
+        Governance
+      </div>
+    </template>
+
+    <template #right>
+      <div class="flex items-center gap-4">
+        <ConnectButton />
+        <HeaderEcosystemMenu />
+      </div>
+    </template>
+  </UHeader>
+</template>
+
+<script setup>
+const links = [
+  {
+    id: "home",
+    label: "Home",
+    to: "/",
+  },
+  {
+    id: "proposals",
+    label: "Proposals",
+    to: "/proposals/all/",
+  },
+  {
+    id: "actors",
+    label: "Actors",
+    to: "/actors/protocol/",
+  },
+  {
+    id: "config",
+    label: "Config",
+    to: "/config/governance/",
+  },
+  {
+    id: "profile",
+    label: "Profile",
+    to: "/profile/me",
+  },
+];
+</script>
