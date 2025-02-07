2482cfbe705f918d487aabc2ed46ecd6f1cd187b
Conrado Cañas <conrado.canas@m0.xyz>
2025-02-07 11:14:18 -0300

    Move images to public folder for better catching

--- components/proposal/ListEmptyState.vue
+++ components/proposal/ListEmptyState.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-80 text-center text-grey-1000">
     <div class="flex justify-center">
-      <img src="~/assets/images/empty-blocks.svg" class="w-40" />
+      <img src="/img/common/empty-blocks.svg" class="w-40" />
     </div>
     <slot />
   </div>
