9fbd9dc919fe15dad03a919475e9b95470f487bb
Jonathan Alvarez <2167222+jonalvarezz@users.noreply.github.com>
2025-02-17 09:44:54 -0500

    cleanup(ProposalCard): remove unused on-view event (#372)
    Signed-off-by: Jonathan Alvarez <2167222+jonalvarezz@users.noreply.github.com>

--- components/proposal/List.vue
+++ components/proposal/List.vue
@@ -2,17 +2,8 @@
   <div>
     <slot v-if="!hasProposals" name="emptyState"></slot>
     <div v-for="proposal in proposals" v-else :key="proposal.proposalId">
-      <ProposalCard
-        :loading="loading"
-        :proposal="proposal"
-        v-bind="$attrs"
-        @on-view="onViewProposal"
-      />
+      <ProposalCard :loading="loading" :proposal="proposal" v-bind="$attrs" />
     </div>
-
-    <MDrawer ref="modal" @on-closed="onCloseDrawer">
-      <ProposalDetails v-if="showProposal" :proposal-id="currentProposal" />
-    </MDrawer>
   </div>
 </template>
 
@@ -31,24 +22,4 @@ const { proposals } = toRefs(props);
 const hasProposals = computed(
   () => proposals.value && proposals.value.length > 0,
 );
-
-const showProposal = ref<boolean>(false);
-const currentProposal = ref<string>();
-const modal = ref();
-
-async function onViewProposal(proposalId: string) {
-  // do not change this oder;
-  await modal.value.close(); //waits for the drawer to unmount
-  showProposal.value = false; // force unmount content of drawer
-  currentProposal.value = proposalId;
-  showProposal.value = true;
-  modal.value.open();
-  window.history.pushState({}, "", `/proposal/${proposalId}`);
-}
-
-function onCloseDrawer() {
-  showProposal.value = false;
-  currentProposal.value = undefined;
-  window.history.replaceState({}, "", "/proposals/");
-}
 </script>
